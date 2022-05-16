e903be0a940ea32ec3b84e0aa3f35d4eb810d60c
spectreascended <astandard87@gmail.com>
2022-05-16 11:00:31 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+// it is common practice to use uppercase for constants that do not change throughout the file inside a configuration file like this.
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = 'e492356a-c08d-404b-921e-1c4b4ac124ae';
+
+export const MODAL_CLOSE_SEC = 2.5;
